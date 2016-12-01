794e264067aa69ca33db976c08162dadbff76765
Florian Wimmenauer <florian.wimmenauer@gmail.com>
2016-12-01 12:04:03 +0100

    login

--- login_post.php
+++ login_post.php
@@ -7,25 +7,23 @@ const pw = "pw";
 try {
     $dbh = Database::getInstance();
     $name = filter_input(INPUT_POST, 'user');
-    $pw = filter_input(INPUT_POST, 'pw');
+    $pw = filter_input(INPUT_POST, 'password');
 
-    $select = $dbh->prepare("SELECT `password`,`grade` FROM `user` WHERE `name` = :name");
+    
+    $select = $dbh->prepare("SELECT `password` FROM `user` WHERE `name` = :name");
     
     $select->bindParam(':name', $name);
    
     $select->execute();
     $readPW;
-    $grade;
     while($row = $select->fetch()){
        $readPW = $row['password'];
-       $grade = $row ['grade'];
     }
   
     if (password_verify($pw, $readPW)) { 
         session_start();
         $_SESSION['logged_in'] = true;
         $_SESSION['name'] = $name;
-        $_SESSION['grade'] = $grade;
         header('Location: userdata.php');
     } else {
         echo "PW OR USER NAME WRONG";
